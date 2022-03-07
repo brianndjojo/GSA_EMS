009a6dd5e2ba5bdc5ddedf44ae1df230167459ff
brianndjojo <38829497+brianndjojo@users.noreply.github.com>
2022-03-08 02:17:37 +0800

    Add files via upload

--- gsaEMS/settings.py
+++ gsaEMS/settings.py
@@ -234,7 +234,6 @@ if not DEBUG:
     ALLOWED_HOSTS = [
         '*',
         'https://gsa-ems-ot87b.ondigitalocean.app/',
-        'gsa-ems-ot87b.ondigitalocean.app/',
         'gsa-ems-ot87b.ondigitalocean.app',
         '127.0.0.1',
         ]
