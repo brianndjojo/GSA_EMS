d13fddaad1d4c5d9f499c8f922fc9877b02514ec
brianndjojo <38829497+brianndjojo@users.noreply.github.com>
2022-03-10 22:05:56 +0800

    Add files via upload

--- events/tests.py
+++ events/tests.py
@@ -3,7 +3,7 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 
-from users.models import User, UserProfile, Event, Signup, Venue, Invoice
+from users.models import User, UserProfile, Event, Signup, Venue
 
 # To be written once development is done..
 # Not recommended though, a better way is to start development with given tests in mind...
